608640833e1b100dd9bd696e03ce220d2ea6e0dd
Emo <eiv.razgrad@gmail.com>
2019-04-19 13:53:53 +0300

    Create project to test AngularSharp - just simple example!

---
+++ TestGetInfo/SimplyExample.cs
@@ -0,0 +1,15 @@
+﻿using AngleSharp;
+
+namespace TestGetInfo
+{
+    public class SimplyExample
+    {
+        var config = Configuration.Default.WithDefaultLoader();
+        var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
+        var context = BrowsingContext.New(config);
+        var document = context.OpenAsync(address);
+        var cellSelector = "tr.vevent td:nth-child(3)";
+        var cells = document.QuerySelectorAll(cellSelector);
+       //var titles = cells.Select(m => m.TextContent);
+    }
+}
---
+++ TestGetInfo/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using AngleSharp;
+using AngleSharp.Html.Parser;
+
+namespace TestGetInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //var config = Configuration.Default.WithDefaultLoader();
+            //var context = BrowsingContext.New(config);
+
+            var parser = new HtmlParser();
+            var webClient = new WebClient(){Encoding = Encoding.GetEncoding("windows-1251") };
+
+            for (int i = 3000; i < 10000; i++)
+            {
+                var url = $"https://fun.dir.bg/vic_open.php?id={i}";
+                var html = webClient.DownloadString(url);
+                var document = parser.ParseDocument(html); //context.OpenAsync(url).GetAwaiter().GetResult();
+                var jokeContent = document.QuerySelector("#newsbody")?.TextContent.Trim();
+                var categoryName = document.QuerySelector(".tag-links-left a")?.TextContent.Trim();
+                Console.WriteLine(jokeContent);
+                Console.WriteLine(categoryName);
+                
+
+            }
+
+           // var cells = document.QuerySelectorAll(cellSelector);
+
+
+
+        }
+    }
+}
